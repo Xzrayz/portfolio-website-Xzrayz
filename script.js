78daf496276e2cb72ca9dcc98596cc359cd26b04
Xzrayz <Donovan.Moua24@student.cart.org>
2025-05-12 10:29:55 -0700

    Commit

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-window.onload = () => {
-  const entrance = document.getElementById("entrance");
-  setTimeout(() => {
-      entrance.style.display = "none";
-      window.location.href = "main.html"; // Change to your main page
-  }, 5000); // Adjust time if needed
-
-  entrance.addEventListener("click", () => {
-      entrance.style.display = "none";
-      window.location.href = "main.html"; // Change to your main page
-  });
-};
 
 
 // GSAP Animations
